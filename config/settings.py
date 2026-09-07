c7bf3adf8c1db26dceb372d213f06723b2ca968a
Sabin Gautam <sabilgautam-fonepoints@Sabins-MacBook-Pro.local>
2026-09-07 16:14:46 +0545

    Revise layout

--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = "xxj1ul-*zq!rjd7gt&qg_%mc!48@t(^$j+3$xr0wb!o"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
